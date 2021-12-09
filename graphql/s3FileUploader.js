9e054ec8a31b6777e93b586f61187174cbd428c2
Bharat Maheshwari <bharat@planetwebsolution.com>
2021-12-09 11:58:30 +0000

    Update s3FileUploader.js

--- graphql/s3FileUploader.js
+++ graphql/s3FileUploader.js
@@ -1,9 +1,9 @@
 const Aws = require('aws-sdk'); 
 
 Aws.config.update({
-  secretAccessKey:'yozM9l4734aDNxi4MpCVmAo4k2kbdvr9Tx8yzAud',
-  accessKeyId:'AKIAUV3FFSC7JRCJM25R',
-  region:'us-west-2'
+  secretAccessKey:'*********************',
+  accessKeyId:'***********',
+  region:'******'
 });
 const s3 = new Aws.S3()
 
